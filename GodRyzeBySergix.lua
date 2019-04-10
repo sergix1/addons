50a9eece69d3d482f4db5aed6b8409d692ff200f
sergix <sergigandia1@gmail.com>
2019-04-10 02:55:41 +0200

    Update GodRyzeBySergix.lua
    done jungleclear laneclear.

--- GodRyzeBySergix.lua
+++ GodRyzeBySergix.lua
@@ -1,5 +1,17 @@
 
-class "Ryze"
+class "Ryze"   
+
+if not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") then
+	print("GsoPred. installed Press 2x F6")
+	DownloadFileAsync("https://raw.githubusercontent.com/gamsteron/GOS-External/master/Common/GamsteronPrediction.lua", COMMON_PATH .. "GamsteronPrediction.lua", function() end)
+	while not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") do end
+end
+
+require('GamsteronPrediction')
+
+
+
+
 
 local LastSpellCast = Game.Timer()
 local forcedTarget
@@ -47,9 +59,6 @@ end
 function Ryze:Tick()
 	if IsRecalling() then return end
 	
-
-	
-	--Orbwalker says we're in combo mode
 	if self:IsComboActive() then 
 		self:Combo()		
 	end	
@@ -57,11 +66,113 @@ function Ryze:Tick()
 	if self:IsHarassActive() then 
 		self:Harass()		
 	end
-	
+	if self:IsLaneClearActive() then 
+		self:LaneClear()
+		self:JungleClear()		
+	end
+end
+function Ryze:IsValidCreep(unit, range)
+    return unit and unit.dead == false and GetDistanceSqr(myHero.pos, unit.pos) <= (range + myHero.boundingRadius + unit.boundingRadius)^2 and unit.isTargetable and unit.isTargetableToTeam and unit.isImmortal == false and unit.visible
+end
+function Ryze:JungleClear() 
+	if Ready(_E) then    
+		for i = 1, Game.MinionCount() do
+			local minion = Game.Minion(i)
+				if self:IsValidCreep(minion, 1000) and minion.team == 300 then
+					if Ryze:HasBuff(minion,"RyzeE") then
+						Control.CastSpell(HK_E,minion) 
+						end
+				end 
+			end
+			for i = 1, Game.MinionCount() do
+				local minion = Game.Minion(i)
+					if self:IsValidCreep(minion, 1000) and minion.team == 300 then
+							Control.CastSpell(HK_E,minion) 
+							
+					end 
+				end
+	end 
+	if Ready(_Q) then    
+		for i = 1, Game.MinionCount() do
+			local minion = Game.Minion(i)
+				if self:IsValidCreep(minion, 1000) and minion.team == 300 then
+					if Ready(_Q) and myHero.pos:DistanceTo(minion.pos) <= Q.Range then
+						if Ryze:HasBuff(minion,"RyzeE") then
+						Control.CastSpell(HK_Q,minion.pos) 
+						end
+					end 
+				end
+			end
+	else
+		for i = 1, Game.MinionCount() do
+			local minion = Game.Minion(i)
+				if self:IsValidCreep(minion, 1000) and minion.team == 300 then
+						Control.CastSpell(HK_W,minion) 
+						end
+					
+				end
+		
+	end
 
 end
 
+function Ryze:LaneClear() 
 
+	
+	if Ready(_E) then    
+
+    for i, minion in ipairs(_G.SDK.ObjectManager:GetEnemyMinions(E.Range)) do
+	if Ryze:HasBuff(minion,"RyzeE") then
+	Control.CastSpell(HK_E,minion) 
+	end
+	for i, minion in ipairs(_G.SDK.ObjectManager:GetEnemyMinions(E.Range)) do
+		Control.CastSpell(HK_E,minion) 
+		end
+     
+end
+
+
+
+
+	end
+	if Ready(_Q) then   
+		for i, minion in ipairs(_G.SDK.ObjectManager:GetEnemyMinions(E.Range)) do
+			if Ryze:HasBuff(minion,"RyzeE") then
+				-- falta el collision con minion
+					Control.CastSpell(HK_Q,minion.pos) 
+
+			end
+		end
+	end
+end
+
+ function Ryze:GetMinionCount(range, pos)
+    local pos = pos
+	local count = 0
+	for i = 1,Game.MinionCount() do
+	local hero = Game.Minion(i)
+	local Range = range * range
+		if hero.team ~= TEAM_ALLY and hero.dead == false and GetDistanceSqr(pos, hero.pos) < Range then
+			count = count + 1
+		end
+	end
+	return count
+end
+function GetDistanceSqr(p1, p2)
+	if not p1 then return math.huge end
+	p2 = p2 or myHero
+	local dx = p1.x - p2.x
+	local dz = (p1.z or p1.y) - (p2.z or p2.y)
+	return dx*dx + dz*dz
+end
+function Ryze:HasBuff(unit, buffName)
+	for i = 0, unit.buffCount do
+		local buff = unit:GetBuff(i);
+	if buff.duration> 0 and buff.name == buffName then
+			return true
+		end
+	end	
+end
 
 function Ryze:Harass()	
 
@@ -72,8 +183,10 @@ function Ryze:Harass()
         then
             if Ready(_Q) 
             then
-             if targetQ:GetCollision(Q.width,Q.speed,Q.delay) == 0 then
-                Control.CastSpell(HK_Q, targetQ) 
+			 if targetQ:GetCollision(Q.width,Q.speed,Q.delay) == 0 then
+				local pred = GetGamsteronPrediction(target, Q, myHero)
+				Control.CastSpell(HK_Q,pred.CastPosition) 
+
              end
               
 
@@ -110,10 +223,12 @@ function Ryze:Combo()
         then
             if Ready(_Q) 
             then
-              --  local Qpos = GetBestCastPosition(targetQ, Q)--
-                Control.CastSpell(HK_Q, target) 
+			  --  local Qpos = GetBestCastPosition(targetQ, Q)--
+			  local pred = GetGamsteronPrediction(target, Q, myHero)
+                Control.CastSpell(HK_Q, pred.CastPosition) 
+			
               
-
+					--
 
             end	
             if Ready(_E)
@@ -137,7 +252,23 @@ function Ryze:Combo()
 	--self:EBounceMinions() --
 end
 
-
+function Ryze:IsLaneClearActive()
+	if _G.SDK and _G.SDK.Orbwalker then		
+		if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_HARASS] then 
+			if myHero.activeSpell and 
+				myHero.activeSpell.valid and 
+				myHero.activeSpell.startTime + myHero.activeSpell.windup - Game.Timer() > 0 
+			then
+				return false
+			else
+				return true
+			end
+		end
+	end	
+	if _G.GOS and _G.GOS.GetMode() == "Clear" and not _G.GOS:IsAttacking() then
+		return true
+	end	
+end
 
 function Ryze:IsHarassActive()
 	if _G.SDK and _G.SDK.Orbwalker then		
